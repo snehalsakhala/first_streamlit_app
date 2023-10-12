256fcbe7e644fb5dcba58ed389271ac8bfacc838
snehalsakhala <49938449+snehalsakhala@users.noreply.github.com>
2023-10-12 15:35:06 +0530

    Update streamlit_app.py
    appending with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
+🥣streamlit.header('Breakfast Menu')
+🥗streamlit.text('Omega 3 & Blueberry Oatmeal')
+🐔streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
