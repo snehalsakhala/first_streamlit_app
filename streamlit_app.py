eb6ed1ed45ea3e0a013538bdac31cd1725a5a88a
snehalsakhala <49938449+snehalsakhala@users.noreply.github.com>
2023-10-12 15:57:59 +0530

    Update streamlit_app.py
    new tittles

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+streamlit.title('My Mom's new healthy diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
+
+
+streamlit.title('Build Your Own Fruit Smoothie')
