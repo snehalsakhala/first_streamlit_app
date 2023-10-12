cb7b4a331ede6121ee1496b6656f94e28ce63ad2
snehalsakhala <49938449+snehalsakhala@users.noreply.github.com>
2023-10-12 15:55:43 +0530

    Update streamlit_app.py
    error with emojis, tried without emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-🥣streamlit.header('Breakfast Favourites')
-🥗streamlit.text('Omega 3 & Blueberry Oatmeal')
-🐔streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Favourites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('Avacado Toast')
