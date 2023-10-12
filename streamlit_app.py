90967890e30982660ab9e3f03c09439b91bdf0d7
snehalsakhala <49938449+snehalsakhala@users.noreply.github.com>
2023-10-12 15:35:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-🥣streamlit.header('Breakfast Menu')
+🥣streamlit.header('Breakfast Favourites')
 🥗streamlit.text('Omega 3 & Blueberry Oatmeal')
 🐔streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
